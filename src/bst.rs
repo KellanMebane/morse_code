4566d6954838fae5bd5c9a2fa5f80e418cf256ba
KellanMebane <kellanmebane@gmail.com>
2018-10-29 14:09:30 -0700

    cleaned up insert/inorder a bit

--- src/bst.rs
+++ src/bst.rs
@@ -5,10 +5,7 @@ pub struct Pair(char, String);
 
 impl Pair {
     pub fn new(c: char, s: String) -> Pair {
-        Pair {
-            0: c,
-            1: s,
-        }
+        Pair { 0: c, 1: s }
     }
 }
 
@@ -42,29 +39,28 @@ impl BST {
         self.internal_insert(0, item)
     }
 
-    fn internal_insert(&mut self, index: usize, item: Pair) -> bool {
-        let pair;
-        {
-            let x = self.vec[index].as_ref();
-            let y = x.as_ref();
-            pair = match y {
-                Some(value) => value.0,
-                None => 0 as char, // null I think lmao
-            };
-            //println!("we are index {}, our char is {}", index, pair);
+    // basically checks if the index is used
+    // returns the char at the index or null char
+    fn get_char(&self, index: usize) -> char {
+        match self.vec[index].as_ref().as_ref() {
+            Some(value) => value.0,
+            None => 0 as char, // null I think lmao
         }
+    }
 
-        //println!("CHECK: we are index {}, our char is {}", index, pair);
+    fn internal_insert(&mut self, index: usize, item: Pair) -> bool {
+        let key = self.get_char(index);
+
         // this means we have an unused index
         // insert here
-        if pair == 0 as char {
+        if key == 0 as char {
             self.vec[index] = Some(item);
             return true;
         }
 
-        if item.0 == pair {
+        if item.0 == key {
             return false;
-        } else if item.0 > pair {
+        } else if item.0 > key {
             let mut new_index = 0;
             if (2 * index) + 2 <= self.size {
                 new_index = (2 * index) + 2;
@@ -77,5 +73,21 @@ impl BST {
             }
             return self.internal_insert(new_index, item);
         }
+    }
+
+    pub fn inorder(&self) {
+        self.internal_inorder(0);
+    }
+
+    fn internal_inorder(&self, index: usize) {
+        let key = self.get_char(index);
+
+        if key == 0 as char {
+            return;
+        }
+
+        self.internal_inorder((2 * index) + 1);
+        println!("{}", key);
+        self.internal_inorder((2 * index) + 2);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,8 @@ fn main() {
     let mut bst = BST::new();
     bst.insert(Pair::new('c', String::from("string")));
     bst.insert(Pair::new('b', String::from("string")));
+    bst.insert(Pair::new('g', String::from("string")));
+    bst.insert(Pair::new('z', String::from("string")));
+    bst.insert(Pair::new('a', String::from("string")));
+    bst.inorder();
 }
