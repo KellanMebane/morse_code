4cb1aada0e4a3bee9fddb547e4f18c6637a48eff
KellanMebane <kellanmebane@gmail.com>
2018-10-30 20:13:40 -0700

    built bst

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
 mod bst;
-//use bst::*;
+use bst::*;
 
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 
 fn main() {
-    //let mut bst = BST::new();
+    let mut bst = BST::new();
     let file = File::open("./src/res/morse.csv").expect("file not found");
 
     for file_line in BufReader::new(file).lines() {
         let line = file_line.unwrap();
         //ignore empty lines
         if line != "" {
-            let data: Vec<&str> = line.split(",").collect();
-
-            println!("{:?}", data);
+            let data: Vec<&str> = line.split(" ").collect();
+            let temp: Vec<_> = data[0].to_string().chars().collect();
+            bst.insert(Pair::new(temp[0], data[1].to_string()));
+            //println!("{:?}", data);
         }
     }
+
+    bst.inorder();
 }
